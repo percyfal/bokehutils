6a8c51d8ce89b9199f742120eeacaf61846c550f
Per Unneberg <per.unneberg@scilifelab.se>
2015-06-10 21:48:31 +0200

    Initial commit
    * setup.cfg: add
    * setup.py: add

---
+++ setup.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2015 by Per Unneberg
+from setuptools import setup, find_packages
+import glob
+import versioneer
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'gaqtk/gaqtk/_version.py'
+versioneer.versionfile_build = 'gaqtk/gaqtk/_version.py'
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'gaqtk-'  # dirname like 'myproject-1.2.0'
+
+setup(name="bokehutils",
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      author="Per Unneberg",
+      author_email="per.unneberg@scilifelab.se",
+      description="Utility functions for working with bokeh plots",
+      license="MIT",
+      scripts=glob.glob('scripts/*.py'),
+      install_requires=[
+          "bokeh",
+          "jinja2",
+          "nose",
+      ],
+      test_suite='nose.collector',
+      packages=find_packages(exclude=['ez_setup', 'test*']),
+      namespace_packages=[
+          'bokehutils',
+      ],
+      package_data={
+          'bokehutils': [
+              'static/*',
+              'templates/*',
+          ],
+      })
