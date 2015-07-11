77daa6c0bc559ecc8396a1fcd72f6a8728c2bc82
Per Unneberg <per.unneberg@scilifelab.se>
2015-07-11 18:15:30 +0200

    Add failure test for title parameter to fig.circle
    Fix #4

--- bokehutils/tests/test_mgeom.py
+++ bokehutils/tests/test_mgeom.py
@@ -1,10 +1,10 @@
 # Copyright (C) 2015 by Per Unneberg
 # pylint: disable=R0904, C0301, C0103, C0111, R0201, E1101, C0325
 import unittest
-from . import data, fig, source
 from nose.tools import raises
-from bokeh.plotting import show, output_file, figure
-from bokehutils.mgeom import mpoints, mlines
+from . import data, fig, source
+from bokeh.plotting import show, output_file
+from bokehutils.mgeom import mpoints, mlines, mdotplot
 
 
 class TestMPoints(unittest.TestCase):
@@ -53,3 +53,36 @@ class TestMLines(unittest.TestCase):
         output_file("tabort.html")
         show(self._fig)
 
+
+class TestMDotplot(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        cls._data = data
+        cls._source = source
+        cls._fig = fig
+
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls._data
+        del cls._source
+        del cls._fig
+
+
+    def test_mdotplot(self):
+        mdotplot(self._fig, "treatment", ["y", "z"],
+                 df=self._data, color=["red", "blue"],
+                 legend=["y", "z"], title="test", title_text_font_size="12pt",
+                 size=10, alpha=0.5)
+        output_file("tabort.html")
+        show(self._fig)
+
+    @raises(AttributeError)
+    def test_mdotplot_title(self):
+        mdotplot(self._fig, x="treatment", y=["x", "y"],
+                 df=self._data, size=10, alpha=0.5,
+                 title="test",title_text_font_size='12pt')
